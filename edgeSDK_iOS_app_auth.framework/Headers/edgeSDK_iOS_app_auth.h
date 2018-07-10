470fd0bd36b8cd9ee377dc87a05cce67d6f717fd
Raduz Benicky <raduz.benicky@mimik.com>
2018-07-10 09:15:40 -0700

    initial pod commit, testing.

---
+++ edgeSDK_iOS_app_auth.framework/Headers/edgeSDK_iOS_app_auth.h
@@ -0,0 +1,19 @@
+//
+//  edgeSDK_iOS_app_auth.h
+//  edgeSDK-iOS-app-auth
+//
+//  Created by Radúz Benický on 2018-07-09.
+//  Copyright © 2018 mimik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for edgeSDK_iOS_app_auth.
+FOUNDATION_EXPORT double edgeSDK_iOS_app_authVersionNumber;
+
+//! Project version string for edgeSDK_iOS_app_auth.
+FOUNDATION_EXPORT const unsigned char edgeSDK_iOS_app_authVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <edgeSDK_iOS_app_auth/PublicHeader.h>
+
+
